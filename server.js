1019de020e7aa92928a2a7c194f9aa8a75f78194
Brian Gershon <brian.five@gmail.com>
2020-09-07 10:31:29 -0700

    Add basic express server.

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log('hello');
+const cool = require("cool-ascii-faces");
+const express = require("express");
+const PORT = process.env.PORT || 5000;
+
+express()
+  .get("/", (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
