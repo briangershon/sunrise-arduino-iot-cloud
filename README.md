1c0d7f83f7050868b8e568f6e97a3c3cf5b2f5e9
Brian Gershon <brian.five@gmail.com>
2020-09-12 18:11:01 -0700

    Add air quality data as well as updated timestamp.

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # sunrise-arduino-iot-cloud
 
-Sends sunrise and sunset times for Seattle, WA to Arduino IoT Cloud.
+Sends sunrise, sunset and air quality data for Seattle WA to Arduino IoT Cloud.
 
-Updates `sunrise` and `sunset` properties in IoT Cloud for DEVICE_ID and THING_ID.
+An Arduino can then grab this information from IoT Cloud and display it.
 
-An Arduino can then grab this information from Arduino IoT Cloud and display it.
+Where does the data come from?
 
-These were calculated by the very cool <https://github.com/mourner/suncalc> library! No API needed.
+* Sunset and sunrise calculated by the very cool <https://github.com/mourner/suncalc> library! No API needed.
+* Air quality data (Ozone and PM2.5) comes from AirNow API <https://docs.airnowapi.org>
 
-Hosted on Heroku at <https://sunrise-arduino-iot-cloud.herokuapp.com>
+Server hosted on Heroku at <https://sunrise-arduino-iot-cloud.herokuapp.com>
 
 ## Heroku Setup
 
@@ -20,11 +21,11 @@ Clone repo then run `heroku git:remote -a sunrise-arduino-iot-cloud` to deploy t
 
 ## Running Locally
 
-Create a local `.env` file with following environment vars to test locally.
+Create a local `.env` file by copying `.envSAMPLE` and then updating all the env vars.
 
     heroku local        # runs local server and loads .env vars
 
-Run command that pushes sunrise to Arduino IoT Cloud:
+Run command that pushes data to Arduino IoT Cloud:
 
     heroku local:run npm run send-sunrise
 
@@ -41,11 +42,15 @@ CLIENT_ID=Arduino_IoT_ClientID_goes_here
 CLIENT_SECRET=Arduino_IoT_ClientSecret_goes_here
 DEVICE_ID=Arduino_IoT_DeviceID_goes_here
 THING_ID=Arduino_IoT_ThingID_goes_here
-SUNRISE_PROPERTY_ID=
-SUNSET_PROPERTY_ID=
+SUNRISE_PROPERTY_ID=property_id_goes_here
+SUNSET_PROPERTY_ID=property_id_goes_here
+OZONE_PROPERTY_ID=property_id_goes_here
+PM25_PROPERTY_ID=property_id_goes_here
+UPDATED_PROPERTY_ID=property_id_goes_here
+AIRNOW_API_KEY=key_from_https://docs.airnowapi.org
 ```
 
-Test sending sunset via:
+Test sending data from server via:
 
     heroku run npm run send-sunrise
 
--- src/send.js
+++ src/send.js
@@ -1,5 +1,6 @@
 const IotApi = require("@arduino/arduino-iot-client");
 const rp = require("request-promise");
+const got = require('got');
 const { calc } = require('./library');
 
 async function getToken() {
@@ -36,8 +37,6 @@ async function run() {
 
     const deviceID = process.env.DEVICE_ID;
     const thingID = process.env.THING_ID;
-    const sunrisePropertyID = process.env.SUNRISE_PROPERTY_ID;
-    const sunsetPropertyID = process.env.SUNSET_PROPERTY_ID;
 
     var thingsAPI = new IotApi.ThingsV2Api(client);
     var opts = {
@@ -49,27 +48,61 @@ async function run() {
     const thingData = await thingsAPI.thingsV2List(opts);
     // console.log(thingData);
     // console.log(thingData[0].properties);
-    // console.log(
-    //   thingData[0].properties.map((p) => {
-    //     return { name: p.name, id: p.id, last_value: p.last_value };
-    //   })
-    // );
+    console.log(
+      thingData[0].properties.map((p) => {
+        return { name: p.name, id: p.id, last_value: p.last_value };
+      })
+    );
 
     const propertiesAPI = new IotApi.PropertiesV2Api(client);
 
-    await propertiesAPI.propertiesV2Publish(thingID, sunrisePropertyID, {
+    await propertiesAPI.propertiesV2Publish(thingID, process.env.SUNRISE_PROPERTY_ID, {
       value: calc().localTimes.sunrise,
     });
-    console.log(`Sent sunrise: '${calc().localTimes.sunrise}' to THING: ${process.env.THING_ID}`);
+    console.log(`Set sunrise to '${calc().localTimes.sunrise}' for ${process.env.THING_ID} thing.`);
 
-    await propertiesAPI.propertiesV2Publish(thingID, sunsetPropertyID, {
+    await propertiesAPI.propertiesV2Publish(thingID, process.env.SUNSET_PROPERTY_ID, {
       value: calc().localTimes.sunset,
     });
-    console.log(`Sent sunset: '${calc().localTimes.sunset}' to THING: ${process.env.THING_ID}`);
+    console.log(`Set sunset to '${calc().localTimes.sunset}' for ${process.env.THING_ID} thing.`);
+
+    const nowFormatted = new Date().toLocaleString("en-US", {
+      timeZone: "America/Los_Angeles",
+    });
+    await propertiesAPI.propertiesV2Publish(thingID, process.env.UPDATED_PROPERTY_ID, {
+      value: nowFormatted,
+    });
+    console.log(`Set updated to '${nowFormatted}' for ${process.env.THING_ID} thing.`);
+
+    const airQuality = await getAirQualityJSON();
+    // console.log(airQuality);
+    const ozoneCategory = airQuality.filter((v) => { return v.ParameterName == 'O3'})[0].Category.Number;
+    const pm25Category = airQuality.filter((v) => { return v.ParameterName == 'PM2.5'})[0].Category.Number;
+
+    await propertiesAPI.propertiesV2Publish(thingID, process.env.OZONE_PROPERTY_ID, {
+      value: ozoneCategory,
+    });
+    console.log(`Set ozoneCategory to ${ozoneCategory} for ${process.env.THING_ID} thing.`);
+
+    await propertiesAPI.propertiesV2Publish(thingID, process.env.PM25_PROPERTY_ID, {
+      value: pm25Category,
+    });
+    console.log(`Set pm25Category to ${pm25Category} for ${process.env.THING_ID} thing.`);
 
   } catch (err) {
     console.log("ERROR", err);
   }
+}
+
+async function getAirQualityJSON() {
+  return got('http://www.airnowapi.org/aq/observation/zipCode/current/', {
+    searchParams: {
+      zipCode: '98104',
+      format: 'application/json',
+      distance: 25,
+      API_KEY: process.env.AIRNOW_API_KEY,
+    }
+  }).json();
 }
 
 run();
